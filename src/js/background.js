172cf251efb12b41178505d9a547b257346e7b45
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-05-29 22:09:11 +0430

    some fixed

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,10 @@
-particlesJS.load('particles-js', '/src/js/particlesjs-config.json', function() {
+let conf = '/src/js/conf/dark-particles-config.json'
+if(!document.getElementsByTagName('html')[0].classList.contains('dark')) {
+    conf = '/src/js/conf/light-particles-config.json'
+}
+
+console.log(conf)
+
+particlesJS.load('particles-js', conf, function() {
     console.log('callback - particles.js config loaded');
 });
