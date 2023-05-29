d191330bb501ba8e6d3411f246d136625cb0f004
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-05-29 20:30:29 +0430

    template completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,js}",
     "./src/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "shabnam-bold": ["shabnam-bold"],
+        "shabnam-medium": ["shabnam-medium"],
+        "mikhak-bold": ["mikhak-bold"],
+        "mikhak-regular": ["mikhak-regular"],
+      },
+      backgroundImage: {
+        "dark-pattern": "url('/static/dark-bg.jpg')",
+        "light-pattern": "url('/static/light-bg.jpg')",
+      }
+    },
   },
   plugins: [],
 }
